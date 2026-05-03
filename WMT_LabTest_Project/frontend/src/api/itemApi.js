414eba609b431af524546be6de60e0f2e34af718
IT24102778 <IT24102778@my.sliit.lk>
2026-05-03 15:39:49 +0530

    update frontend

--- WMT_LabTest_Project/frontend/src/api/itemApi.js
+++ WMT_LabTest_Project/frontend/src/api/itemApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://wmt-labtest-4-sr1k.onrender.com/api",
 });
 
 export const getItems = () => API.get("/items");
